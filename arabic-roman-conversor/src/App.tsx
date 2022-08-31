fa05409eed364d823ac476ab127ea76070ff0487
GTI/WILLYAM.PIMENTEL <10482656417@sdspe.infra>
2022-08-31 14:30:00 -0300

    feat: adding react app

---
+++ arabic-roman-conversor/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { RomanNumbersConversorPage } from './pages/RomanNumbersConversorPage/'
+
+function App() {
+
+  return (
+    <div className="App">
+      <RomanNumbersConversorPage />
+      
+    </div>
+  )
+}
+
+export default App
